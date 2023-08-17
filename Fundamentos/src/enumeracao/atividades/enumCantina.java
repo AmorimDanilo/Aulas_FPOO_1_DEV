f5071a1cf4925426f6ded29fd4bf7543235c997a
Danilo Sousa <danilosousa@aluno.senai.br>
2023-08-17 10:49:06 -0300

    Enviando foto de enumeracao

---
+++ Fundamentos/src/enumeracao/atividades/enumCantina.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao.atividades;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum enumCantina {
+    BEBIDAS,
+    SALGADOS,
+    DOCES,
+    PRATO_FEITO,
+    LANCHES_ARTESANAIS,
+    SOBREMESAS,
+    ACOMPANHAMENTOS,
+    OPCOES_VEGETARIANAS
+}
